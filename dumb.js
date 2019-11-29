345352eb7b82a0c0130ef1fc93b9f2c1e544d703
SheepTester <ussa.say@gmail.com>
2019-11-29 14:46:55 -0800

    generate list of words

---
+++ dumb.js
@@ -0,0 +1,27 @@
+s = ''
+
+official = 'n mod sep vt vi interj prep conj kama cont conj oth'.split(' ')
+$$('dt').forEach(dt => {
+const term = /[a-z]+(, [a-z]+)*/.exec(dt.textContent)[0]
+const defs = [...dt.parentNode.querySelectorAll('dd[lang=en] i')]
+const off = []
+const add = []
+defs.map(a => {
+const defin = a.parentNode.textContent
+const definit = defin.slice(defin.indexOf(' '))
+if (official.includes(a.textContent)) {
+off.push([a.textContent, definit])
+} else {
+add.push([a.textContent, definit])
+}
+})
+s += `${term}:
+  off:
+${off.map(([a, b]) => `    ['${a}', '${b}']`).join('\n')}
+  add:
+${add.map(([a, b]) => `    ['${a}', '${b}']`).join('\n')}
+
+`
+
+})
+s
